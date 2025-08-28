271a8cfb63e13d5629b3357f02831fbf882ddb59
architechpro <65943221+architechpro@users.noreply.github.com>
2025-08-28 20:48:09 +0000

    Update documentation link for database driver in fallback-cache configuration

--- src/Config/fallback-cache.php
+++ src/Config/fallback-cache.php
@@ -7,7 +7,7 @@ return [
      * `database` is recommended here, but for compatibility reasons it defaults to `array`.
      *
      * Note: `database` driver requires migration.
-     * @see https://laravel.com/docs/10.x/cache#prerequisites-database
+     * @see https://laravel.com/docs/12.x/cache#prerequisites-database
      */
     Configuration::FALLBACK_CACHE_STORE => env(
         'FALLBACK_CACHE_STORE',
