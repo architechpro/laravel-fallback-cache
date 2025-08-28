1f27bfbd076c3a523b341469dd12c3c13ba254b1
architechpro <65943221+architechpro@users.noreply.github.com>
2025-08-28 16:35:36 +0000

    updated composer, ensured tests work

--- src/FallbackCacheServiceProvider.php
+++ src/FallbackCacheServiceProvider.php
@@ -6,27 +6,23 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Support\DeferrableProvider;
 use LaravelFallbackCache\Config\Configuration;
 use Throwable;
 
-class FallbackCacheServiceProvider extends ServiceProvider implements DeferrableProvider
+class FallbackCacheServiceProvider extends ServiceProvider
 {
     public const CONFIG_CACHE_DEFAULT = 'cache.default';
 
     /**
      * @return void
      */
-    public function register(): void
-    {
-        if (!$this->isCacheStoreHealthy()) {
-            $this->switchToFallbackCacheStore();
-        }
-    }
-
     public function boot(): void
     {
         $this->publishConfig();
+
+        if (!$this->isCacheStoreHealthy()) {
+            $this->switchToFallbackCacheStore();
+        }
     }
 
     /**
@@ -46,35 +42,21 @@ class FallbackCacheServiceProvider extends ServiceProvider implements Deferrable
     private function isCacheStoreHealthy(): bool
     {
         try {
-            $store = Config::get(self::CONFIG_CACHE_DEFAULT);
-            
-            if ($store === 'redis') {
-                $redis = Cache::store('redis')->getRedis();
-                
-                // First check if Redis is responding
-                $redis->ping();
-                
-                // Then try to perform a read operation
-                $redis->get('health_check_key');
-            } else {
-                // For non-Redis stores, try a simple get operation
-                Cache::store($store)->get('health_check_key');
-            }
-            
-            return true;
+            Cache::get('');
         } catch (Throwable $exception) {
             Log::error(
                 'Cache store is unhealthy',
                 [
                     'exception' => get_class($exception),
                     'message'   => $exception->getMessage(),
-                    'trace'     => $exception->getTraceAsString(),
-                    'store' => $store ?? Config::get(self::CONFIG_CACHE_DEFAULT)
+                    'trace'     => $exception->getTraceAsString()
                 ]
             );
 
             return false;
         }
+
+        return true;
     }
 
     /**
